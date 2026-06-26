15dd9c41f79f836dc9e75567760111e4e84f8a81
Sahildevta30 <suryasahilsskg@gmail.com>
2026-06-26 09:03:00 +0530

    Fix suspense boundary error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
+  output: 'standalone',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 module.exports = nextConfig;
